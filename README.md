c3c06db800f4a3328765fe7463804c5dae55b452
ALEJANDRO BARRERA FLORES <wavey.dev0@gmail.com>
2022-08-23 18:43:55 -0500

    refactor | mobile responsive | styles tweak

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a template for a landing page <br>
+This site is fully desktop and mobile responsive <br>
+Site includes light/dark mode toggle as well as language selector for English and Spanish<br>
+Icons, images and theme colors were obtained from frontendmentor.io<br>
--- js/index.js
+++ js/index.js
@@ -9,9 +9,39 @@ $(".enToggle").click(() => {
 })
 
 $(".modeToggle").click(() => {
-    $(".homeSection,.servicesSection,.teamSection").toggleClass("darkBG1");
-    $(".aboutSection,.portfolioSection,.contactSection").toggleClass("darkBG2");
-    $("header").toggleClass("headerDark");
-    $(".section,.navbar li a").toggleClass("darkText");
+    $(".homeSection, .servicesSection, .contactSection").toggleClass("darkBG1");
+    $(".aboutSection, .teamSection").toggleClass("darkBG2");
+    $("header, .sideMenu").toggleClass("headerDark");
+    $(".section,.navbar li a, .menuNav a").toggleClass("darkText");
     $(".modeToggle").toggleClass("darkbtn");
+})
+$(".menuBtn").click(() => {
+    $(".sideMenu").toggleClass("menuActive")
+})
+$(".section").click(() => {
+    $(".sideMenu").removeClass("menuActive")
+})
+
+// GSAP ANIMATIONS
+gsap.from(".homeS2 img", {
+    x:"20%",
+    opacity: "0",
+    duration: 2,
+    ease: "power1.in"
+})
+gsap.from(".homeS2", {
+    backgroundPosition: "100%",
+    duration: 2,
+    ease: "power1.in"
+})
+gsap.from(".homeS1", {
+    x:"-10%",
+    opacity: "0",
+    duration: 2,
+    ease: "power1.in"
+})
+gsap.from(".aboutSection", {
+    opacity: "0",
+    duration: 4,
+    ease: "power1.in"
 })
